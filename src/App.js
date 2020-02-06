113373334793d7fd80756dcd2f0524b94370df0d
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2020-02-06 21:18:27 +0200

    add Book component

--- src/App.js
+++ src/App.js
@@ -78,28 +78,38 @@ class Bookshelf extends React.Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
             {books.map(book => (
-              <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors.join(', ')}</div>
-                  </div>
-              </li>
+              <Book key={book.id} book={book} />
             ))}
           </ol>
         </div>
       </div>
+    )
+  }
+}
+
+class Book extends React.Component {
+  render() {
+    const { book } = this.props;
+
+    return (
+      <li>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-shelf-changer">
+                <select>
+                  <option value="move" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors.join(', ')}</div>
+          </div>
+      </li>
     )
   }
 }
